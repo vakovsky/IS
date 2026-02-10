ecebf70b585cf00fcfa0c85590c7ebde8dde1da5
Dimiter Vakovsky <vakovsky@gmail.com>
2026-02-10 11:21:24 +0200

    Create des.cs

---
+++ des.cs
@@ -0,0 +1,41 @@
+ public class DES
+ {
+     public static string Encrypt(string plainText, string key, string iv)
+     {
+         using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+         {
+             des.Key = Encoding.UTF8.GetBytes(key);
+             des.IV = Encoding.UTF8.GetBytes(iv);
+
+             byte[] bytes = Encoding.UTF8.GetBytes(plainText);
+
+             using (MemoryStream ms = new MemoryStream())
+             using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+             {
+                 cs.Write(bytes, 0, bytes.Length);
+                 cs.FlushFinalBlock();
+
+                  return Convert.ToBase64String(ms.ToArray());
+             }
+         }
+     }
+     public static string Decrypt(string cipherText, string key, string iv)
+     {
+         using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+         {
+             des.Key = Encoding.UTF8.GetBytes(key);
+             des.IV = Encoding.UTF8.GetBytes(iv);
+
+             byte[] bytes = Convert.FromBase64String(cipherText);
+
+             using (MemoryStream ms = new MemoryStream())
+             using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+             {
+                 cs.Write(bytes, 0, bytes.Length);
+                 cs.FlushFinalBlock();
+
+                 return Encoding.UTF8.GetString(ms.ToArray());
+             }
+         }
+     }
+ }
